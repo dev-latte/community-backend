c94abca22301a31129c9ab3c5202732cbc2eb45f
dev-latte <dev.vlatte@gmail.com>
2022-04-28 22:27:53 +0900

    Initial commit

---
+++ src/main/java/com/commu/back/communitybackend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.commu.back.communitybackend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CommunityBackendApplication.class);
+	}
+
+}
---
+++ src/test/java/com/commu/back/communitybackend/CommunityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commu.back.communitybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
