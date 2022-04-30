3963acfa9ae1d48b109ccc9782d147673a81a516
dev-latte <dev.vlatte@gmail.com>
2022-04-30 19:38:06 +0900

    1) h2 설정 추가 2) 검색, 수정 API 추가 3) cors 해결

--- src/main/java/com/commu/back/communitybackend/service/posts/PostsService.java
+++ src/main/java/com/commu/back/communitybackend/service/posts/PostsService.java
@@ -1,6 +1,9 @@
 package com.commu.back.communitybackend.service.posts;
 
+import com.commu.back.communitybackend.domain.posts.Posts;
 import com.commu.back.communitybackend.domain.posts.PostsRepository;
+import com.commu.back.communitybackend.web.dto.PostsResponseDto;
+import com.commu.back.communitybackend.web.dto.PostsUpdateRequestDto;
 import com.commu.back.communitybackend.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,24 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository
+                        .findById(id)
+                        .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository
+                        .findById(id)
+                        .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 
 }
--- src/test/java/com/commu/back/communitybackend/web/PostsApiControllerTest.java
+++ src/test/java/com/commu/back/communitybackend/web/PostsApiControllerTest.java
@@ -2,6 +2,7 @@ package com.commu.back.communitybackend.web;
 
 import com.commu.back.communitybackend.domain.posts.Posts;
 import com.commu.back.communitybackend.domain.posts.PostsRepository;
+import com.commu.back.communitybackend.web.dto.PostsUpdateRequestDto;
 import com.commu.back.communitybackend.web.dto.PostsSaveRequestDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -32,22 +35,22 @@ public class PostsApiControllerTest {
     private PostsRepository postsRepository;
 
     @AfterEach
-    public void tearDown() throws Exception{
+    public void tearDown() throws Exception {
         postsRepository.deleteAll();
     }
 
     @Test
-    public void Posts_등록된다(){
+    public void Posts_등록된다() {
         // given
         String title = "title";
         String content = "content";
         PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
-                                                                .title(title)
-                                                                .content(content)
-                                                                .author("author")
-                                                                .build();
+                .title(title)
+                .content(content)
+                .author("author")
+                .build();
 
-        String url = "http://localhost:" + port + "api/v1/posts";
+        String url = "http://localhost:" + port + "/api/v1/posts";
 
         // when
         ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
@@ -59,5 +62,39 @@ public class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void Posts_수정된다() {
+        // given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long updateId = savedPosts.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+
     }
 }
