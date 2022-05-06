fb1d3f81db706cc9697edccbc5bb2cc65679e72b
dev-latte <dev.vlatte@gmail.com>
2022-05-06 21:51:48 +0900

    resolve cors error

--- src/main/java/com/commu/back/communitybackend/config/auth/SecurityConfig.java
+++ src/main/java/com/commu/back/communitybackend/config/auth/SecurityConfig.java
@@ -1,6 +1,7 @@
-package com.commu.back.communitybackend.config;
+package com.commu.back.communitybackend.config.auth;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,8 +17,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeRequests()
+                        .mvcMatchers(HttpMethod.OPTIONS).permitAll()
                         .antMatchers("/", "/login/**", "/twitter/complete", "/h2-console/**").permitAll()
-                        .anyRequest().authenticated()
+//                        .anyRequest().authenticated()
                 .and()
                 .logout()
                     .logoutUrl("/logout")
